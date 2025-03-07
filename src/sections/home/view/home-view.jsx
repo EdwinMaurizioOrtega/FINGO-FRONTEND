6b56fc05418cc1371d57533e1ee34c115cef569d
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2025-03-07 17:17:11 -0500

    Pixel FB

--- src/sections/home/view/home-view.jsx
+++ src/sections/home/view/home-view.jsx
@@ -132,6 +132,26 @@ export function HomeView() {
           `}
       </Script>
 
+      <Script strategy="afterInteractive">
+        {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2561848230670314');
+            fbq('track', 'PageView');
+          `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }} loading="lazy"
+             src="https://www.facebook.com/tr?id=2561848230670314&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <ScrollProgress
         variant="linear"
         progress={pageProgress.scrollYProgress}
